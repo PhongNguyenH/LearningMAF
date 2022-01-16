35405f75c64d097ff8bf37223d783ca39e6c41f5
bacngao <bacngao@gmail.com>
2022-01-16 11:26:29 +0700

    Update runFeature.sh

--- runFeature.sh
+++ runFeature.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 mkdir -p test/report
 npx preprocessor
-
 npx cucumber-js -f json:test/report/cucumber_report.json --require "features/**/*.js" tmp/features/$*; 
 val=$?
 npx multiReport
-openPy $PWD/test/report/undefined/index.html
 exit $val
