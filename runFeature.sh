48308df9265f27c9a92015180e68f6af58b2e933
bacngao <bacngao@gmail.com>
2022-01-16 16:17:11 +0700

    Update runFeature.sh

--- runFeature.sh
+++ runFeature.sh
@@ -2,6 +2,6 @@ mkdir -p ./test/report
 node node_modules/@cucumber/cucumber/bin/cucumber-js -f json:test/report/report.json ./features/MyLearning.feature
 #start chrome.exe %cd%/test/report/index.html
 result=$?
-echo "result = $result"
+#echo "result = $result"
 #node node_modules/@ln-maf/core/multiReport.js
 exit $result
